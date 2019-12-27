acb1b52d5d76bb3b64e790a4a94d2c7e495e0d09
eric42827 <b06502147@ntu.edu.tw>
2019-12-27 23:21:08 +0800

    io complete

--- PA3/src/main.cpp
+++ PA3/src/main.cpp
@@ -4,7 +4,10 @@
 #include <vector>
 using namespace std;
 
-int ind(int i,int j,int num){return i*num+j;}
+int ind(int i,int j,int num){return i*num+j;}//num for total num (this is n*n mat)
+/*
+-100<=w<=100
+*/
 
 struct CycleBreaker
 {
@@ -12,28 +15,46 @@ struct CycleBreaker
     int n = 0;  //numOfVertices
     int m = 0;  //numOfEdges
     vector<uint16_t> edges; //denote which vertex connect
-    vector<uint16_t> weights;   //denote weight of vertices
+    //vector<uint16_t> weights;   //denote weight of vertices
+    ofstream ofs;
 
-    CycleBreaker(const char *inputFile){
+    CycleBreaker(const char *inputFile){ //constructor
         ifstream ifs(inputFile);
         if(!ifs)
             return;
         ifs >> graphType;
         ifs >> n;
         ifs >> m;
-        uint16_t a = 0,b = 0,w = 0;
-        edges.assign(n,0);
-        for(; m > 0; m--){
-            ifs >> a >> b;
-            edges[a] = b;
-            weights[a] = w;
+        
+        edges.assign(n*n,UINT16_MAX);
+        //weights.assign(n*n,0);
+        for(int i = 0; i < m; i++){
+            uint16_t a = 0,b = 0,w = 0;
+            ifs >> a >> b >> w;
+            edges[ind(a,b,n)] = w;
+            //weights[a] = w;
             if(graphType != 'd'){
-                edges[b] = a;
-                weights[b] = w;
+                edges[ind(b,a,n)] = w;
             }
         }
+    }
 
+    void output(const char *outputFile){//outputfile
+        ofs.open(outputFile);
+        ofs << graphType << endl;
+        ofs << n << endl;
+        ofs << m << endl;
+        
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                if(edges[ind(i,j,n)]<=100&&edges[ind(i,j,n)]>=-100)
+                    ofs << i << " " << j << " " << edges[ind(i,j,n)] << endl;
+            }
+        }
+         
 
+        ofs.flush();
+        ofs.close();
     }
     
 };
@@ -41,5 +62,6 @@ struct CycleBreaker
 
 int main(int argc, char *argv[]){
     CycleBreaker breaker(argv[1]);
+    breaker.output(argv[2]);
     return 0;
 }
